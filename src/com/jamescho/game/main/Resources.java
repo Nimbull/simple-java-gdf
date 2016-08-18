096c79868ecc1fb6c88dcfd656af2b92e1e955b5
Sean <cfbmoo1@hotmail.com>
2016-08-18 12:45:10 -0400

    Updated resources class.

--- src/com/jamescho/game/main/Resources.java
+++ src/com/jamescho/game/main/Resources.java
@@ -2,6 +2,7 @@ package com.jamescho.game.main;
 
 import java.applet.Applet;
 import java.applet.AudioClip;
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.net.URL;
 
@@ -11,10 +12,20 @@ public class Resources {
 	// Public members.
 	public static BufferedImage welcome;
 	public static BufferedImage iconImage;
+	public static BufferedImage line;
+	public static AudioClip hit;
+	public static AudioClip bounce;
+	public static Color darkBlue;
+	public static Color darkRed;
 
 	public static void load() {
 		welcome = loadImage("welcome.png");
 		iconImage = loadImage("iconimage.png");
+		line = loadImage("line.png");
+		hit = loadSound("hit.wav");
+		bounce = loadSound("bounce.wav");
+		darkBlue = new Color(25, 83, 105); // Accepts RGB values.
+		darkRed = new Color(105, 13, 13); // Accepts RGB values.
 	}
 
 	private static AudioClip loadSound(String inFileName) {
