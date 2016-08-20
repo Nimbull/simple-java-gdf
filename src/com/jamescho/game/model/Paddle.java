fd102d07cd652d4fb82c000bfb772b9f3352b143
Sean <cfbmoo1@hotmail.com>
2016-08-20 10:35:12 -0400

    Adding paddles.

--- src/com/jamescho/game/model/Paddle.java
+++ src/com/jamescho/game/model/Paddle.java
@@ -13,6 +13,7 @@ public class Paddle {
 	private int height;
 	private int velY;
 	private Rectangle rect;
+	private final static int MOVE_SPEED_Y = 4;
 	
 	/**
 	 * <p>Constructor.</p>
@@ -29,5 +30,86 @@ public class Paddle {
 		this.height = inHeight;
 		this.rect = new Rectangle(this.x, this.y, this.width, this.height);
 		this.velY = 0;
+	}
+
+	/**
+	 * <p>Performs a velocity update for this rectangle in the game.</p>
+	 */
+	public void update() {
+		this.y += this.velY;
+		updateRect();
+	}
+
+	/**
+	 * <p>Updates the rectangle bounds.</p>
+	 */
+	private void updateRect() {
+		this.rect.setBounds(this.x, this.y, this.width, this.height);
+	}
+
+	/**
+	 * <p>Accelerate up method.</p>
+	 */
+	public void accelUp() {
+		this.velY = -Paddle.MOVE_SPEED_Y;
+	}
+
+	/**
+	 * <p>Accelerate down method.</p>
+	 */
+	public void accelDown() {
+		this.velY = Paddle.MOVE_SPEED_Y;
+	}
+
+	/**
+	 * <p>Stop paddle movement.</p>
+	 */
+	public void stop() {
+		this.velY = 0;
+	}
+
+	/**
+	 * <p>Gets the X position of the paddle.</p>
+	 * 
+	 * @return int that is the X position.
+	 */
+	public int getX() {
+		return this.x;
+	}
+
+	/**
+	 * <p>Gets the Y position of the paddle.</p>
+	 * 
+	 * @return int that is the Y position.
+	 */
+	public int getY() {
+		return this.y;
+	}
+
+	/**
+	 * <p>Gets the width of the paddle.</p>
+	 * 
+	 * @return int that is the width.
+	 */
+	public int getWidth() {
+		return this.width;
+	}
+
+	/**
+	 * <p>Gets the height of the paddle.</p>
+	 * 
+	 * @return int that is the height.
+	 */
+	public int getHeight() {
+		return this.height;
+	}
+
+	/**
+	 * <p>Gets the Rectangle object of the paddle.</p>
+	 * 
+	 * @return Rectangle is the Rectangle object of the paddle.
+	 */
+	public Rectangle getRect() {
+		return this.rect;
 	}
 }
--- src/com/jamescho/game/state/PlayState.java
+++ src/com/jamescho/game/state/PlayState.java
@@ -1,18 +1,25 @@
 package com.jamescho.game.state;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 import com.jamescho.game.main.GameMain;
 import com.jamescho.game.main.Resources;
+import com.jamescho.game.model.Paddle;
 
 public class PlayState extends State {
+	// Private members.
+	private Paddle paddleLeft;
+	private Paddle paddleRight;
+	private static final int PADDLE_WIDTH = 15;
+	private static final int PADDLE_HEIGHT = 60;
 
 	@Override
 	public void init() {
-		// TODO Auto-generated method stub
-		
+		this.paddleLeft = new Paddle(0, 195, PlayState.PADDLE_WIDTH, PlayState.PADDLE_HEIGHT);
+		this.paddleRight = new Paddle(785, 195, PlayState.PADDLE_WIDTH, PlayState.PADDLE_HEIGHT);
 	}
 
 	@Override
@@ -29,8 +36,16 @@ public class PlayState extends State {
 		g.setColor(Resources.darkRed);
 		g.fillRect(GameMain.GAME_WIDTH / 2, 0, GameMain.GAME_WIDTH / 2,
 				GameMain.GAME_HEIGHT);
+
 		// Draw line.
 		g.drawImage(Resources.line, (GameMain.GAME_WIDTH / 2) - 2, 0, null);
+
+		// Draw paddles.
+		g.setColor(Color.WHITE);
+		g.fillRect(this.paddleLeft.getX(), this.paddleLeft.getY(),
+				this.paddleLeft.getWidth(), this.paddleLeft.getHeight());
+		g.fillRect(this.paddleRight.getX(), this.paddleRight.getY(),
+				this.paddleRight.getWidth(), this.paddleRight.getHeight());
 	}
 
 	@Override
