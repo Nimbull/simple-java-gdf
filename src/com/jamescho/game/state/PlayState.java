aab8d87a077940d1dd1e5d80983c7aa1f910132a
Sean <cfbmoo1@hotmail.com>
2016-08-23 11:05:25 -0400

    Adding score UI.

--- src/com/jamescho/game/state/PlayState.java
+++ src/com/jamescho/game/state/PlayState.java
@@ -1,6 +1,7 @@
 package com.jamescho.game.state;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -15,11 +16,15 @@ public class PlayState extends State {
 	private Paddle paddleRight;
 	private static final int PADDLE_WIDTH = 15;
 	private static final int PADDLE_HEIGHT = 60;
+	private int scoreLeft = 0;
+	private int scoreRight = 0;
+	private Font scoreFont;
 
 	@Override
 	public void init() {
 		this.paddleLeft = new Paddle(0, 195, PlayState.PADDLE_WIDTH, PlayState.PADDLE_HEIGHT);
 		this.paddleRight = new Paddle(785, 195, PlayState.PADDLE_WIDTH, PlayState.PADDLE_HEIGHT);
+		this.scoreFont = new Font("SansSerif", Font.BOLD, 25);
 	}
 
 	@Override
@@ -46,6 +51,11 @@ public class PlayState extends State {
 				this.paddleLeft.getWidth(), this.paddleLeft.getHeight());
 		g.fillRect(this.paddleRight.getX(), this.paddleRight.getY(),
 				this.paddleRight.getWidth(), this.paddleRight.getHeight());
+
+		// Draw UI.
+		g.setFont(this.scoreFont);
+		g.drawString(new Integer(this.scoreLeft).toString(), 350, 40);
+		g.drawString(new Integer(this.scoreRight).toString(), 435, 40);
 	}
 
 	@Override
